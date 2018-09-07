e59edef8b1f258ecd65fc5e878349131f40faae7
Benjamin Thurston <benjamin.thurston87@gmail.com>
2018-09-07 14:03:45 -0400

    Add files via upload

---
+++ calc.rb
@@ -0,0 +1,24 @@
+def add(a, b)
+	puts a + b
+end
+
+def +(a, b)
+	puts a + b
+end
+
+puts "Welcome to calculator"
+puts "What is the first operand"
+op_a = gets.chomp.to_f
+
+puts "What is the second operand"
+op_b = gets.chomp.to_f
+
+puts "What operand do you want to use?"
+operator = gets.chomp
+
+if operator == 'add'
+	add(op_a, op_b)
+end
+if operator == '+'
+	add(op_a, op_b)
+end
